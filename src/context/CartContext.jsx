40cb914dfaa285b51bd503fe327138e755e1710d
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2025-09-17 20:32:01 +0000

    Aula 17/09 - Supabase config

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, createContext } from "react";
+import { supabase } from '../utils/supabase';
 
 export const CartContext = createContext({
   // Context to manage the products state
@@ -14,28 +15,40 @@ export const CartContext = createContext({
 });
 
 export function CartProvider({ children }) {
-  // State to manage products
-  var category = "smartphones";
-  var limit = 10;
-  var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
-
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        setProducts(data.products);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+    async function fetchProductsSupabase() {
+      const { data, error } = await supabase
+        .from('product_2v')
+        .select();
+      if (error) {
+        setError(`Fetching products failed! ${error.message}`);
+      } else {
+        setProducts(data);
       }
+      setLoading(false);
     }
-    fetchProducts();
+    fetchProductsSupabase();
+    // State to manage products API
+    // var category = "smartphones";
+    // var limit = 10;
+    // var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
+
+    // async function fetchProducts() {
+    //   try {
+    //     const response = await fetch(apiUrl);
+    //     const data = await response.json();
+    //     setProducts(data.products);
+    //   } catch (error) {
+    //     setError(error);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // }
+    // fetchProducts();
   }, []);
 
   // State to manage the cart
@@ -47,7 +60,7 @@ export function CartProvider({ children }) {
     if (existingProduct) {
       updateQtyCart(product.id, existingProduct.quantity + 1);
     } else {
-      setCart((prevCart) => [...prevCart, {...product, quantity: 1}]);
+      setCart((prevCart) => [...prevCart, { ...product, quantity: 1 }]);
     }
   }
 
